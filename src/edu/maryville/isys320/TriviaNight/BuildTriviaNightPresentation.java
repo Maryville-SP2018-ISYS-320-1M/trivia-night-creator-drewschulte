bbc9eadbee90289022939d78c7cd3210611874dd
drewschulte <37315081+drewschulte@users.noreply.github.com>
2018-05-02 21:24:32 -0500

    save

--- src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
+++ src/edu/maryville/isys320/TriviaNight/BuildTriviaNightPresentation.java
@@ -2,17 +2,16 @@ package edu.maryville.isys320.TriviaNight;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class BuildTriviaNightPresentation {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String fileName = "//Users//joedent//TriviaNight.pptx";
+		String fileName = "TriviaNight.pptx";
 		SlideShowBuilder builder = new SlideShowBuilder();
 		QuestionsManager qm = new QuestionsManager("data");
 		
 		builder.buildSlideShow(qm.getCategories(), qm.getQuestions(), qm.getAnswers(), fileName);
-//		builder.printSlideShowInfo("Sample.pptx");
+		//builder.printSlideShowInfo("TriviaNight.pptx");
 
 	}
 	
